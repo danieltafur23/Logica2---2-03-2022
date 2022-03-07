3b9e83b30ab224ff884393d84481c84b2b8dfd1e
danieltafur23 <79168401+danieltafur23@users.noreply.github.com>
2022-03-07 16:29:38 -0500

    Create Examen.java

---
+++ Examen.java
@@ -0,0 +1,14 @@
+package empleado;
+
+public class Examen {
+    public static void main(String[] args) {
+        Empleado e = new Empleado("Pedro", "123", 30, true, 4000000);
+        Programador  p1 = new Programador("Sandra", "234", 20, false, 30000, 120, "Java");
+        
+        System.out.println("Nombre del empleado: " + e.getNombre());
+        System.out.println("Sueldo del empleado con aumento del 10%: " + e.aumentoSalario(10));
+        
+        System.out.println("Nombre del empleado: " + p1.getNombre());
+        System.out.println("Sueldo del prpgramador con aumento del 10%: " + p1.aumentoSalario(10));
+    }
+}
